7a07f74d78485de6a466faa174e83da8abf636b5
EndocrineMSc <bamsc.studio@gmail.com>
2023-10-27 10:36:56 +0200

    Added Loading Screen

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,14 +8,48 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class LoadingScreen : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _loadingText;
+
+    const string _loading1 = "Laden.";
+    const string _loading2 = "Laden..";
+    const string _loading3 = "Laden...";
+
+    private readonly float _textTimerMax = 1f;
+    private float _textTimer;
+    
     private void Start()
     {
         StartCoroutine(WaitForLoadingScreen());
+        _textTimer = _textTimerMax;
+        _loadingText.text = _loading1;
     }
 
     private IEnumerator WaitForLoadingScreen()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(LoadHelper.LoadDuration);
         SceneManager.LoadScene(LoadHelper.SceneToBeLoaded.ToString());
+    }
+
+    private void Update()
+    {
+        _textTimer -= Time.deltaTime;
+
+        if (_textTimer <= 0)
+        {
+            switch (_loadingText.text)
+            {
+                case _loading1:
+                    _loadingText.text = _loading2;
+                    break;
+                case _loading2:
+                    _loadingText.text = _loading3;
+                    break;
+                case _loading3:
+                    _loadingText.text = _loading1;
+                    break;
+            }
+
+            _textTimer = _textTimerMax;
+        }
     }
 }
